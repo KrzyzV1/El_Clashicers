25bc572e34f3d84aad2dc70e4789ee1b57083116
KrzyzV1 <damian20022@onet.pl>
2025-09-02 20:46:58 +0200

    initial

---
+++ backend/src/test/java/pl/elclashicers/ElClashicersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.elclashicers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElClashicersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
